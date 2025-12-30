bc9c65e8c4107042a67255da42ac7ddbd0d83490
lakshman-nanchari <lakshmannanchari96@gmail.com>
2025-12-30 18:07:59 +0530

    feat: add basic home page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+    return(
+        <main className="p-6">
+            <h1 className="text-xl font-semibold">
+                Token Trading Table
+            </h1>
+        </main>
+    )
+}
